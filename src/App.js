d0acb09946b60506d319816f94e8653e935bf1b6
jun-sato445 <jun.fms445@gmail.com>
2021-03-08 06:44:17 +0900

    テスト commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello world!!
-        </h1>
-      </header>
-    </div>
-  );
+//function App() {
+//  return (
+//    <div>Hello world!!</div>
+//  );
+//}
+
+class App extends Component {
+  render() {
+    return <h1>Hello world!!</h1>;
+  }
 }
 
 export default App;
