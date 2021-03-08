dd8217a2840518f2014430d7ead1f7f26539fc9c
jun-sato445 <jun.fms445@gmail.com>
2021-03-09 05:50:23 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -7,15 +7,40 @@ import './App.css';
 //  );
 //}
 
-class App extends Component {
-  render() {
-    return (
-    <React.Fragment>
-    <label htmlFor = "bar">bar </label>
-    <input type="text" onClikck={() => {console.log("Hi!!")}} />
-    </React.Fragment>
-    )
-  }
+//class App extends Component {
+//  render() {
+//    return (
+//    <React.Fragment>
+//    <label htmlFor = "bar">bar </label>
+//    <input type="text" onClikck={() => {console.log("Hi!!")}} />
+//    </React.Fragment>
+//    )
+//  }
+//}
+
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: "10"},
+    { name: "Hanako", age: "5"},
+    { name: "Noname"}
+  ]
+  return (
+    <div>
+      {
+      profiles.map((profile,index) => {
+        return <User name={profile.name} age={profile.age} key={index} />      
+      })
+      }
+    </div>
+  )
+}
+
+const User = (Props) => {
+  return <div>Hi, I am {Props.name}! and {Props.age} years old</div>
+}
+
+User.defaultProps = {
+  age:1
 }
 
 export default App;
